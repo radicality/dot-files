3b30c8027422c5c736d942d5f790600bfd68452f
Rafal <rs@vlixter.com>
2010-11-03 11:06:16 +0000

    Changes....

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln ~/dot-files/.tmux.conf ~/.tmux.conf
 ln -s ~/dot-files/.vim ~/.vim
 ln -s ~/dot-files/.vimperator ~/.vimperator
 ln -s ~/dot-files/.pentadactyl ~/.pentadactyl
+
 ln ~/dot-files/.vimrc ~/.vimrc
 ln -s ~/dot-files/.zsh ~/.zsh
 ln ~/dot-files/.zshrc ~/.zshrc
