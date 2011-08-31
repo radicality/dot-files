852f17cad72ebbfe541c9d801e9ed4b1ebb66223
Rafal Szymanski <me@rafal.io>
2011-08-31 21:53:04 +0100

    new config setup

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-ln ~/dot-files/.tmux.conf ~/.tmux.conf
-ln -s ~/dot-files/.vim ~/.vim
-ln -s ~/dot-files/.vimperator ~/.vimperator
-ln -s ~/dot-files/.pentadactyl ~/.pentadactyl
+ln -s ~/dot-files/vim ~/.vim
+ln -s ~/dot-files/vim/vimrc ~/.vimrc
+ln -s ~/dot-files/zsh/ ~/.zsh
+ln -s ~/dot-files/zsh/zshrc ~/.zshrc
 
-ln ~/dot-files/.vimrc ~/.vimrc
-ln -s ~/dot-files/.zsh ~/.zsh
-ln ~/dot-files/.zshrc ~/.zshrc
-ln ~/dot-files/.vimperatorrc ~/.vimperatorrc
-ln ~/dot-files/.pentadactylrc ~/.pentadactylrc
