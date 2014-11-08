9f6be1a1a77f35caa0fc76f87a2c565db8c340ba
Rafal Szymanski <me@rafal.io>
2014-11-08 12:39:45 -0800

    adding xvimrc

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ ln -s ~/dot-files/zsh/ ~/.zsh
 ln -s ~/dot-files/zsh/zshrc ~/.zshrc
 ln -s ~/dot-files/ghci ~/.ghci
 ln -s ~/dot-files/gitconfig ~/.gitconfig
+ln -s ~/dot-files/xvimrc ~/.xvimrc
 
 cp extra/tomorrow-night-eighties.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
