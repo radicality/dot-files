baf855e4b733ba6a7617a32cf91200722e042749
Rafal Szymanski <me@rafal.io>
2015-05-21 22:30:03 -0400

    tarsnap and some cleanup

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ ln -s ~/dot-files/zsh/zshrc ~/.zshrc
 ln -s ~/dot-files/ghci ~/.ghci
 ln -s ~/dot-files/gitconfig ~/.gitconfig
 ln -s ~/dot-files/xvimrc ~/.xvimrc
+ln -s ~/dot-files/tarsnaprc ~/.tarsnaprc
+ln -s ~/dot-files/tmux.conf ~/.tmux.conf
 
 cp extra/tomorrow-night-eighties.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
