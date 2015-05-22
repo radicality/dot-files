55c2a012420f2f799ea59410eee4cb7f03546d2a
Rafal Szymanski <me@rafal.io>
2015-05-21 22:38:24 -0400

    get rid of line

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ ln -s ~/dot-files/vim/vimrc ~/.vimrc
 ln -s ~/dot-files/zsh/ ~/.zsh
 ln -s ~/dot-files/zsh/zshrc ~/.zshrc
 ln -s ~/dot-files/ghci ~/.ghci
-
 ln -s ~/dot-files/gitconfig ~/.gitconfig
 ln -s ~/dot-files/xvimrc ~/.xvimrc
 ln -s ~/dot-files/tarsnaprc ~/.tarsnaprc
