84ba35653341768d90e208b2d57a90eb4626b0e2
Rafal Szymanski <me@rafal.io>
2016-01-23 14:55:15 -0500

    Assorted changes to dot-files.

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-ln -s ~/dot-files/vim ~/.vim
-ln -s ~/dot-files/vim/vimrc ~/.vimrc
-ln -s ~/dot-files/zsh/ ~/.zsh
-ln -s ~/dot-files/zsh/zshrc ~/.zshrc
-ln -s ~/dot-files/ghci ~/.ghci
-ln -s ~/dot-files/gitconfig ~/.gitconfig
-ln -s ~/dot-files/xvimrc ~/.xvimrc
-ln -s ~/dot-files/tarsnaprc ~/.tarsnaprc
-ln -s ~/dot-files/tmux.conf ~/.tmux.conf
+ln -sf ~/dot-files/vim ~/.vim
+ln -sf ~/dot-files/vim/vimrc ~/.vimrc
+ln -sf ~/dot-files/zsh/ ~/.zsh
+ln -sf ~/dot-files/zsh/zshrc ~/.zshrc
+ln -sf ~/dot-files/ghci ~/.ghci
+ln -sf ~/dot-files/gitconfig ~/.gitconfig
+ln -sf ~/dot-files/xvimrc ~/.xvimrc
+ln -sf ~/dot-files/tarsnaprc ~/.tarsnaprc
+ln -sf ~/dot-files/tmux.conf ~/.tmux.conf
+ln -sf ~/dot-files/dircolors ~/.dircolors
 
 cp extra/tomorrow-night-eighties.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
